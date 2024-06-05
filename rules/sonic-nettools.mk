478a418a86697c68ff2af0e5ed5546d36798167f
Wenda Chu <32250288+w1nda@users.noreply.github.com>
2024-06-05 16:47:46 +0800

    Update sonic-nettools.mk

--- rules/sonic-nettools.mk
+++ rules/sonic-nettools.mk
@@ -2,6 +2,8 @@ SONIC_NETTOOLS_VERSION = 0.1
 
 export SONIC_NETTOOLS_VERSION
 
-SONIC_NETTOOLS = sonic-nettools.deb
+SONIC_NETTOOLS = sonic-nettools_$(SONIC_NETTOOLS_VERSION)_$(CONFIGURED_ARCH).deb
 $(SONIC_NETTOOLS)_SRC_PATH = $(SRC_PATH)/sonic-nettools
 SONIC_MAKE_DEBS += $(SONIC_NETTOOLS)
+
+$(eval $(call add_extra_package,$(SONIC_NETTOOLS),$(package)))
