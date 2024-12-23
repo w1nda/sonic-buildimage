c56e6ca0d54567af5ac45d1da54f7f946a3c5d7e
wenda <wendachu@microsoft.com>
2024-12-23 09:24:56 +0000

    patch sonic-mgmt docker image

--- rules/ptf-py3.mk
+++ rules/ptf-py3.mk
@@ -2,7 +2,6 @@
 
 PTF_PY3 = ptf-0.10.0.post0-py3-none-any.whl
 $(PTF_PY3)_SRC_PATH = $(SRC_PATH)/ptf-py3
-$(PTF_PY3)_PATCH_PATH = $(SRC_PATH)/ptf-py3.patch
 $(PTF_PY3)_PYTHON_VERSION = 3
 $(PTF_PY3)_TEST = n
 SONIC_PYTHON_WHEELS += $(PTF_PY3)
